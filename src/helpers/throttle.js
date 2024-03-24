651bba81a178ed14135a51e7d228829e6f8921e7
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-03-25 00:22:11 +0700

    Добавление хелпера throttle

---
+++ src/helpers/throttle.js
@@ -0,0 +1,14 @@
+export default function throttle(callee, timeout) {
+  let timer = null
+
+  return function perform(...args) {
+    if (timer) return
+
+    timer = setTimeout(() => {
+      callee(...args)
+
+      clearTimeout(timer)
+      timer = null
+    }, timeout)
+  }
+}
